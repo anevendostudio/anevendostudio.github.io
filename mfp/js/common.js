1f4eea7337aa706d16021edfc8107801f8ac7963
anevendostudio <anevendostudio@gmail.com>
2017-05-30 03:57:36 +0300

    new

--- mfp/js/common.js
+++ mfp/js/common.js
@@ -1,5 +1,31 @@
 $(function() {
 
 	// Custom JS
+	//Toggle Mnu
+	$(".toggle-mnu").click(function() {
+		$(this).toggleClass("on");
+		$(this).parent().next().next().find(".main-mnu").slideToggle();
+		return false;
+	});
+
+	$(".main-foot .toggle-mnu").click(function() {
+		$("html, body").animate({scrollTop: $(document).height() + 500}, "slow");
+		return false;
+	});
+
+  (function() {
+    var cx = '012144409076897957623:dnagzos1kf4';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+
+
+
+
+
 
 });
